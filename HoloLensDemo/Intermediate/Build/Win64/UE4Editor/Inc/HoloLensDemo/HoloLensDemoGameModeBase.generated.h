00e9ba3d06d6a5c52713676aad444831818ee22b
Janelle Domantay <janelledomantay@gmail.com>
2022-11-24 20:26:13 -0600

    Initial Commit

---
+++ HoloLensDemo/Intermediate/Build/Win64/UE4Editor/Inc/HoloLensDemo/HoloLensDemoGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef HOLOLENSDEMO_HoloLensDemoGameModeBase_generated_h
+#error "HoloLensDemoGameModeBase.generated.h already included, missing '#pragma once' in HoloLensDemoGameModeBase.h"
+#endif
+#define HOLOLENSDEMO_HoloLensDemoGameModeBase_generated_h
+
+#define HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_SPARSE_DATA
+#define HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_RPC_WRAPPERS
+#define HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAHoloLensDemoGameModeBase(); \
+	friend struct Z_Construct_UClass_AHoloLensDemoGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AHoloLensDemoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/HoloLensDemo"), NO_API) \
+	DECLARE_SERIALIZER(AHoloLensDemoGameModeBase)
+
+
+#define HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAHoloLensDemoGameModeBase(); \
+	friend struct Z_Construct_UClass_AHoloLensDemoGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AHoloLensDemoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/HoloLensDemo"), NO_API) \
+	DECLARE_SERIALIZER(AHoloLensDemoGameModeBase)
+
+
+#define HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AHoloLensDemoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AHoloLensDemoGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AHoloLensDemoGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AHoloLensDemoGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AHoloLensDemoGameModeBase(AHoloLensDemoGameModeBase&&); \
+	NO_API AHoloLensDemoGameModeBase(const AHoloLensDemoGameModeBase&); \
+public:
+
+
+#define HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AHoloLensDemoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AHoloLensDemoGameModeBase(AHoloLensDemoGameModeBase&&); \
+	NO_API AHoloLensDemoGameModeBase(const AHoloLensDemoGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AHoloLensDemoGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AHoloLensDemoGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AHoloLensDemoGameModeBase)
+
+
+#define HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_12_PROLOG
+#define HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_SPARSE_DATA \
+	HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_RPC_WRAPPERS \
+	HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_INCLASS \
+	HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_SPARSE_DATA \
+	HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> HOLOLENSDEMO_API UClass* StaticClass<class AHoloLensDemoGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HoloLensDemo_Source_HoloLensDemo_HoloLensDemoGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ HoloLensDemo/Source/HoloLensDemo/HoloLensDemoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HoloLensDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOLOLENSDEMO_API AHoloLensDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
